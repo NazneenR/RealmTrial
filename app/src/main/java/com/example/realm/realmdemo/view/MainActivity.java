1490646bf2ccc3ceda79f384d27ed5bca1a581f3
NazneenRupawalla <nazneenr@thoughtworks.com>
2016-12-14 01:22:20 +0530

    Shows the time in milliseconds taken for various operations.

--- app/src/main/java/com/example/realm/realmdemo/view/MainActivity.java
+++ app/src/main/java/com/example/realm/realmdemo/view/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.realm.realmdemo.view;
 
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.LinearLayout;
@@ -49,40 +50,65 @@ public class MainActivity extends AppCompatActivity {
   }
 
   private void basicCRUD() {
-    showStatus("Perform basic Create/Read/Update operations...");
+    long startTime = SystemClock.currentThreadTimeMillis();
+    showStatus("Perform basic create operations...");
     repository.addApartmentWithRent(7500);
+    long endTime = SystemClock.currentThreadTimeMillis();
+    long diff = endTime - startTime;
+    showStatus("Basic create operations :- " + diff + " milliseconds");
 
+    long startTimeForUpdate = SystemClock.currentThreadTimeMillis();
+    showStatus("Perform update...");
     repository.updateFirstApartmentInTheList();
-    showStatus("Rent increased");
+    long endTimeForUpdate = SystemClock.currentThreadTimeMillis();
+    long diffForUpdate = endTimeForUpdate - startTimeForUpdate;
+    showStatus("Basic Update operations :- " + diffForUpdate + " milliseconds");
   }
 
   private void basicQuery() {
+    long startTime = SystemClock.currentThreadTimeMillis();
     showStatus("\nPerforming basic Query operation...");
     RealmResults<Apartment> results = repository.fetchApartmentsBasedOnRentValue(8000);
 
+    long endTime = SystemClock.currentThreadTimeMillis();
+    long diff = endTime - startTime;
+
     showStatus("Size of result set: " + results.size());
+    showStatus("Basic query :- " + diff + " milliseconds");
   }
 
   private void basicLinkQuery() {
+    long startTime = SystemClock.currentThreadTimeMillis();
     showStatus("\nPerforming basic Link Query operation...");
 
     RealmResults<Apartment> results = repository.fetchApartmentsBasedOnName("Hermes Heritage");
+    long endTime = SystemClock.currentThreadTimeMillis();
+    long diff = endTime - startTime;
 
     showStatus("Size of result set: " + results.size());
+    showStatus("Basic link query :- " + diff + " milliseconds");
   }
 
   private void complexReadWrite() {
+    long startTime = SystemClock.currentThreadTimeMillis();
     showStatus("\nPerforming complex Read/Write operation...");
 
     for (int i = 7500; i < 10000; i++) {
       repository.addApartmentWithRent(i);
     }
+    long endTime = SystemClock.currentThreadTimeMillis();
+    long diff = endTime - startTime;
+    showStatus("Complex read and write :- " + diff + " milliseconds");
   }
 
   private void complexQuery() {
+    long startTime = SystemClock.currentThreadTimeMillis();
     showStatus("\n\nPerforming complex Query operation...");
 
     RealmResults<Apartment> results = repository.executeComplexQuery();
+    long endTime = SystemClock.currentThreadTimeMillis();
+    long diff = endTime - startTime;
     showStatus("\nSize of result set: " + results.size());
+    showStatus("Complex link :- " + diff + " milliseconds");
   }
 }
